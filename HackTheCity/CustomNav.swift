594aeeecd4ac268a3badc7c6ad712df54a0d8190
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-01-09 16:12:24 -0500

    update

---
+++ HackTheCity/CustomNav.swift
@@ -0,0 +1,21 @@
+//
+//  CustomNav.swift
+//  HackTheCity
+//
+//  Created by Vidyadhar V. Thatte on 1/9/16.
+//  Copyright © 2016 Thatte. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavBar: UINavigationController {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
